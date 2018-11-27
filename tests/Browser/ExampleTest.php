98c07786a8d6a2d6b4cb4d449f8665e40d6c8805
Jess Archer <jess@jessarcher.com>
2018-11-27 21:58:46 +1000

    Add Laravel Dusk

---
+++ tests/Browser/ExampleTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ExampleTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Lister');
+        });
+
+        $user = factory(User::class)->create([
+            'email' => 'jess@jessarcher.com',
+        ]);
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/login')
+                    ->type('email', $user->email)
+                    ->type('password', 'secret')
+                    ->press('Login')
+                    ->assertPathIs('/home');
+        });
+    }
+}
