53c53a65653bc361dc4e6f24fe55f8a19d26d69d
Jess Archer <jess@jessarcher.com>
2018-12-12 23:29:43 +1000

    Update service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,50 +1,39 @@
 const cacheName = 'lister-cache';
-const isExcluded = f => /hot-update|sockjs/.test(f);
 
 const filesToCache = [
-    //...serviceWorkerOption.assets.filter(file => !isExcluded(file)),
-    '/',
-    '/home',
-    '/home#/',
-    '/manifest.json',
-    '/js/manifest.js',
-    '/js/vendor.js',
-    '/js/app.js',
-    '/css/app.css',
-]
+    './',
+    './home',
+    './manifest.json',
+    './js/manifest.js',
+    './js/vendor.js',
+    './js/app.js',
+    './css/app.css',
+];
 
-// Store a response inside the cache
-const updateCache = (request, response) =>
-  caches.open(cacheName).then(cache => cache.put(request, response));
+self.addEventListener('install', (event) => {
+    console.log('The service worker is being installed.');
 
-const fetchFromNetwork = request =>
-    new Promise((resolve, reject) => {
-        fetch(request).then(response => {
-            if (!isExcluded(request.url) && response) {
-                updateCache(request, response.clone()).then(() => resolve(response));
-            } else {
-                resolve(response);
-            }
-        }, reject);
-    });
-
-// Try to fetch existing responses from the cache
-const fetchFromCache = request =>
-  caches.match(request).then(response => response || Promise.reject('failed'));
-
-// Cache known assets up-front
-const preCache = () =>
-    caches.open(cacheName).then(cache => {
-        cache.addAll(filesToCache);
-    });
-
-// Handle the 'install' event
-self.addEventListener('install', event => {
-    event.waitUntil(preCache());
+    event.waitUntil(precache());
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        fetchFromNetwork(event.request).catch(() => fetchFromCache(event.request))
-    );
+self.addEventListener('fetch', (event) => {
+    console.log('The service worker is serving the asset.');
+
+    event.respondWith(fromNetwork(event.request, 400).catch(() => fromCache(event.request)));
+};
+
+const precache = () => caches.open(cacheName).then((cache) => cache.addAll(filesToCache));
+
+const fromNetwork = (request, timeout) => new Promise((fulfill, reject) => {
+    var timeoutId = setTimeout(reject, timeout);
+
+    fetch(request).then((response) => {
+        clearTimeout(timeoutId);
+        fulfill(response);
+    }, reject);
 });
+
+const fromCache = (request) =>
+    caches.open(cacheName)
+        .then((cache) => cache.match(request)
+            .then((matching) => matching || Promise.reject('no-match'));
