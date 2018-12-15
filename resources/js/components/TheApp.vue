67aaf482959fc0669d875c83244b152892900e0b
Jess Archer <jess@jessarcher.com>
2018-12-15 22:52:13 +1000

    More UI tweaks

--- resources/js/components/TheApp.vue
+++ resources/js/components/TheApp.vue
@@ -2,6 +2,17 @@
     <div>
         <nav class="bg-purple shadow text-white flex justify-between px-4 py-2">
             <router-link to="/" class="text-purple-lightest no-underline">Home</router-link>
+
+            <a
+                href="/logout"
+                onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                class="text-purple-lightest no-underline"
+            >
+                Logout
+            </a>
+            <form id="logout-form" action="/logout" method="POST" style="display: none;">
+                <input type="hidden" name="_token" :value="csrf">
+            </form>
         </nav>
 
         <main>
@@ -12,6 +23,13 @@
 
 <script>
 export default {
+    computed: {
+        csrf() {
+            const token = document.head.querySelector('meta[name="csrf-token"]');
+            return token.content;
+        }
+    },
+
     created() {
         this.$store.dispatch('lists/fetch');
     },
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,70 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="manifest" href="{{ url('manifest.json') }}">
-        <meta name="theme-color" content="#317EFB">
+@extends('layouts.main')
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+@section('content')
 
-        <title>{{ config('app.name', 'Lister') }}</title>
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            height: 100vh;
+            margin: 0;
+        }
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        .full-height {
+            height: 100vh;
+        }
 
-        <script src="{{ mix('js/manifest.js') }}" defer></script>
-        <script src="{{ mix('js/vendor.js') }}" defer></script>
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .full-height {
-                height: 100vh;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+    <div class="flex-center position-ref full-height">
+        @if (Route::has('login'))
+            <div class="top-right links">
+                @auth
+                    <a href="{{ url('/app') }}">Home</a>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
 
-            .content {
-                text-align: center;
-            }
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
 
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/app') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Lister
-                </div>
+        <div class="text-center">
+            <div class="text-5xl mb-6">
+                Lister
             </div>
         </div>
-        <div id="app"></div>
-    </body>
-</html>
+    </div>
+
+@endsection
