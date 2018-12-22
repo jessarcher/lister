8c37b506f4b19a589bfe803327056f5357c358a6
Jess Archer <jess@jessarcher.com>
2018-12-22 23:56:17 +1000

    Enable background sync process

--- src-sw.js
+++ src-sw.js
@@ -1,10 +1,20 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 
-console.log('this is my custom service worker');
-
 workbox.precaching.precacheAndRoute([]);
 
 workbox.routing.registerRoute(
     new RegExp('.*'),
     workbox.strategies.networkFirst()
+);
+
+const bgSyncPlugin = new workbox.backgroundSync.Plugin('myQueueName', {
+    maxRetentionTime: 24 * 60 // Retry for max of 24 Hours
+});
+
+workbox.routing.registerRoute(
+    /\/api\/.*/,
+    workbox.strategies.networkOnly({
+        plugins: [bgSyncPlugin]
+    }),
+    'POST'
 );
