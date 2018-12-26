214c6cd7d8e57c584708952458028eed1705df0f
Jess Archer <jess@jessarcher.com>
2018-12-26 18:19:28 +1000

    Encapsulate autosize.js in <autosize-textarea> component

---
+++ resources/js/components/AutosizeTextarea.vue
@@ -0,0 +1,19 @@
+<template>
+    <textarea ref="textarea" :value="$slots.default[0].text" rows="1" @blur="blur"></textarea>
+</template>
+
+<script>
+import autosize from "autosize"
+
+export default {
+    mounted() {
+        autosize(this.$refs.textarea)
+    },
+
+    methods: {
+        blur(e) {
+            this.$emit('blur', e)
+        },
+    },
+}
+</script>
