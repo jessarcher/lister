e0fa8249cbe22b34d3c3c4b438e303309a897673
Jess Archer <jess@jessarcher.com>
2018-12-26 21:13:41 +1000

    Silently fail when no response received

--- resources/js/helpers.js
+++ resources/js/helpers.js
@@ -11,8 +11,8 @@ export function handleAxiosError (error) {
         // The request was made but no response was received
         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
         // http.ClientRequest in node.js
-        alert('Error with request');
-        console.error(error.request);
+
+        // Silently fail as background sync in the service worker should retry this
     } else {
         // Something happened in setting up the request that triggered an Error
         alert('Unknown error');
