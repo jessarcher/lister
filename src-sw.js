fde50b86126f9ea87c2227a584308cf5e5167959
Jess Archer <jess@jessarcher.com>
2018-12-27 11:22:15 +1000

    Update service worker for background sync and cache-first

--- src-sw.js
+++ src-sw.js
@@ -1,20 +1,29 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 
-workbox.precaching.precacheAndRoute([]);
-
-workbox.routing.registerRoute(
-    new RegExp('.*'),
-    workbox.strategies.networkFirst()
-);
+// Background Sync for POST/PUT/PATCH/DELETE to API
 
 const bgSyncPlugin = new workbox.backgroundSync.Plugin('myQueueName', {
     maxRetentionTime: 24 * 60 // Retry for max of 24 Hours
 });
 
+['POST', 'PUT', 'PATCH', 'DELETE'].forEach(httpMethod => {
+    workbox.routing.registerRoute(
+        new RegExp('/api/.*'),
+        workbox.strategies.networkOnly({
+            plugins: [bgSyncPlugin]
+        }),
+        httpMethod
+    )
+})
+
+// Network first for GET to API
+
 workbox.routing.registerRoute(
-    /\/api\/.*/,
-    workbox.strategies.networkOnly({
-        plugins: [bgSyncPlugin]
-    }),
-    'POST'
+    new RegExp('/api/.*'),
+    workbox.strategies.networkFirst(),
+    'GET'
 );
+
+// Cache first everything else (js, css, fonts, images)
+
+workbox.precaching.precacheAndRoute([]);
