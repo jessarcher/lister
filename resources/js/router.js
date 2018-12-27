d6e47b62199a8d7101ad6094fa8de341e21af13c
Jess Archer <jess@jessarcher.com>
2018-12-27 11:07:47 +1000

    Route / to an empty home component

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Home from './components/Home';
+import ListItems from './components/ListItems';
+
 Vue.use(VueRouter)
 
 export default new VueRouter({
     routes: [
-        { path: '/', component: require('./components/Lists') },
-        { path: '/lists/:list_id', component: require('./components/ListItems') },
+        { path: '/', component: Home },
+        { path: '/lists/:list_id', component: ListItems },
     ],
 });
