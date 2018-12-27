9f7e0348af4df90f653fd0426854094b0a30c9aa
Jess Archer <jess@jessarcher.com>
2018-12-27 11:21:37 +1000

    Configure workbox to cache unminified assets for testing locally

--- workbox-config.js
+++ workbox-config.js
@@ -4,5 +4,6 @@ module.exports = {
     "**/*.{css,ico,eot,svg,ttf,woff,woff2,png,php,js,json,html,txt}"
   ],
   "swSrc": "src-sw.js",
-  "swDest": "public/sw.js"
+  "swDest": "public/sw.js",
+  "maximumFileSizeToCacheInBytes": 5000000
 };
